293a728bd453a9afd9a2182ee01b91162092ec9f
praveenmethraskar <praveenmethraskar@gmail.com>
2022-08-05 12:57:36 +0530

    Swap Two Numbers

---
+++ Day_fifth_Assignment/SwapTwoNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_fifth_Assignment
+{
+    public class SwapTwoNumbers
+    {
+        public void Swapping()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Enter Two Numbers To Swap a and b");
+
+            int a = Int32.Parse(Console.ReadLine());
+            int b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Before swap a= "+a+" b= "+b);
+
+            a=a* b; //a=50 (5*10)      
+            b=a/b; //b=5 (50/10)      
+            a=a/b; //a=10 (50/5)    
+
+            Console.Write("After swap a= "+a+" b= "+b);
+        }
+    }
+}
