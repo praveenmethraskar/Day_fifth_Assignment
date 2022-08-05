670517cf0b7dc5208071ef0bdec0c668150a7c44
praveenmethraskar <praveenmethraskar@gmail.com>
2022-08-05 12:28:00 +0530

    Harmonic Number

--- Day_fifth_Assignment/Harmonic.cs
+++ Day_fifth_Assignment/Harmonic.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Day_fifth_Assignment
 {
-    internal class Harmonic
+    public class Harmonic
     {
+        public void Harmonic1()
+        {
+            int i, n;
+            double s = 0.0;
+
+            Console.Write("\n\n");
+            Console.WriteLine("Calculate the harmonic series and their sum:\n");
+            Console.Write("----------------------------------------------");
+            Console.Write("\n\n");
+
+            Console.WriteLine("Input the number of terms : ");
+            n= Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n\n");
+            for (i=1; i<=n; i++)
+            {
+                Console.Write("1/{0} + ", i);
+                s+=1/(float)i;
+            }
+            Console.Write("\nSum of Series upto {0} terms : {1} \n", n, s);
+        }
     }
 }
