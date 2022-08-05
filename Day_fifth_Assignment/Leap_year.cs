308a84a2c7a000f70b182243cc76f182da2219a8
praveenmethraskar <praveenmethraskar@gmail.com>
2022-08-05 12:03:19 +0530

    Leap Year

---
+++ Day_fifth_Assignment/Leap_year.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_fifth_Assignment
+{
+    public class Leap_year
+    {
+        public void Leap_Year()
+        {
+            Console.WriteLine("Enter Year in format of yyyy");
+            int year = Int32.Parse(Console.ReadLine());
+
+            if (((year % 4==0) && (year % 100 !=0)) || (year % 400 == 0))
+            {
+                Console.WriteLine("{0} is a leap year");
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a leap year");
+            }
+        }
+    }
+}
--- Day_fifth_Assignment/Program.cs
+++ Day_fifth_Assignment/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Day_fifth_Assignment
+{
+    public class Program
+    {
+        void FlipCoin()
+        {
+            Console.WriteLine("Enter How many times to flip coin");
+            double n = Double.Parse(Console.ReadLine());
+
+
+            int h_count = 0;
+            int t_count = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                Random random = new Random();
+                int head_or_tail = random.Next(0, 2);
+                if (head_or_tail == 1)
+                {
+                    Console.WriteLine("Heads");
+                    h_count++;
+                }
+                else
+                {
+                    Console.WriteLine("Tails");
+                    t_count++;
+                }
+            }
+            Console.WriteLine("no of heads "+h_count);
+            Console.WriteLine("no of tails "+t_count);
+
+            double Head_percentage = (h_count/n)*100;
+            double Tail_Percentage = (t_count/n)*100;
+
+            Console.WriteLine("Percentage of Heads is = {0}%", Head_percentage);
+            Console.WriteLine("Percentage of Tails is = {0}% ", Tail_Percentage);
+
+        }
+
+        public static void Main(string[] args)
+        {
+            Program obj = new Program();
+
+
+            Console.WriteLine("Flip coin problem");
+            obj.FlipCoin();
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Leap_year leap_Year = new Leap_year();
+            leap_Year.Leap_Year();
+        }
+    }
+}
