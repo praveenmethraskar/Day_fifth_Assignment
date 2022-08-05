8e32aeaa3ddac7c3b5fe7425d25a75241a539d1e
praveenmethraskar <praveenmethraskar@gmail.com>
2022-08-05 13:04:24 +0530

    Vowels

---
+++ Day_fifth_Assignment/Alphabet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_fifth_Assignment
+{
+    public class Alphabet
+    {
+        public void Vowels()
+        {
+            Console.WriteLine("Enter an Alphabet A-Z or a-z");
+            char ch = Convert.ToChar(Console.ReadLine().ToLower());
+
+            switch (ch)
+            {
+                case 'a':
+                    Console.WriteLine("{0} is Vowel", ch);
+                    break;
+                case 'e':
+                    Console.WriteLine("{0} is Vowel", ch);
+                    break;
+                case 'i':
+                    Console.WriteLine("{0} is Vowel", ch);
+                    break;
+                case 'o':
+                    Console.WriteLine("{0} is Vowel", ch);
+                    break;
+                case 'u':
+                    Console.WriteLine("{0} is Vowel", ch);
+                    break;
+                default:
+                    Console.WriteLine("{0} is Consonent", ch);
+                    break;
+            }
+
+
+        }
+    }
+}
