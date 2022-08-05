6b5cbf8f02f28c1fe604936d7841db11f922fb4f
praveenmethraskar <praveenmethraskar@gmail.com>
2022-08-05 12:52:59 +0530

    Quotient and Remainder

---
+++ Day_fifth_Assignment/RemainderAndQuotient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_fifth_Assignment
+{
+    public class RemainderAndQuotient
+    {
+        public void Division_Problem()
+        {
+            Console.WriteLine("Enter Dividend and Divisor value to Quotient and Remainder ");
+            int dividend = Int32.Parse(Console.ReadLine());
+            int divisor = Int32.Parse(Console.ReadLine());
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine(" Dividend : {0} and Divisor : {1} ", dividend, divisor);
+            Console.WriteLine("Quotinet = " +quotient);
+            Console.WriteLine("Remaider = " +remainder);
+
+        }
+    }
+}
