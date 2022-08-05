2204b0e0df44739ac27ec1330e93c179f03d9aa5
praveenmethraskar <praveenmethraskar@gmail.com>
2022-08-05 13:00:56 +0530

    EvenOrOdd

---
+++ Day_fifth_Assignment/EvenOrOdd.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_fifth_Assignment
+{
+    public class EvenOrOdd
+    {
+        public void Even_Or_Odd()
+        {
+            Console.WriteLine("Enter a Number to find it is Even or Odd");
+
+            int num = Int32.Parse(Console.ReadLine());
+            int rem;
+
+            rem = num % 2;
+
+            if (rem == 0)
+            {
+                Console.WriteLine("{0} Entered Number is Even", num);
+            }
+            else
+            {
+                Console.WriteLine("{0} Entered Number is odd", num);
+            }
+
+        }
+    }
+}
