a84a5f25b4b6fe68de2b67bd7864b5f11fb7db42
praveenmethraskar <55995179+praveenmethraskar@users.noreply.github.com>
2022-08-10 20:22:35 +0530

    Update Power_Of_2.cs

--- Day_fifth_Assignment/Power_Of_2.cs
+++ Day_fifth_Assignment/Power_Of_2.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Day_fifth_Assignment
 {
-    internal class Power_Of_2
+public class Power_Of_2
     {
+        public void Pow_2()
+        {
+            Console.WriteLine("Enter  any positive number to know power of 2");
+            int power = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("your find 2 power {0}", power);
+            double pow_ab = 0;
+
+
+
+            for (int i = 1; i <= power; i++)
+            {
+                pow_ab = Math.Pow(2, power);
+            }
+
+            Console.WriteLine("the power of 2 is " + pow_ab);
+        }
     }
 }
